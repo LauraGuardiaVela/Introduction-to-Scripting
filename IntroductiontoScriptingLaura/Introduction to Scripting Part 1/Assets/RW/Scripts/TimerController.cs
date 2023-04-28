d95d76793062cb0f51164e92128a50ff2238ca1f
LauraGuardiaVela <92033923+LauraGuardiaVela@users.noreply.github.com>
2023-04-28 19:26:36 +0200

    Introduction To Scripting

---
+++ IntroductiontoScriptingLaura/Introduction to Scripting Part 1/Assets/RW/Scripts/TimerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerController : MonoBehaviour
+{
+    public float timer = 60f;
+    public Text timerText;
+    private int setTimerInt;
+
+    private GameStateManager gameStateManager; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameStateManager = FindObjectOfType<GameStateManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0){
+            timer -= Time.deltaTime;
+        } else {
+            timer = 0;
+            gameStateManager.GameOver(); 
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene("Title");
+            }
+        }
+
+        setTimerInt = (int)timer;
+
+        timerText.text = setTimerInt.ToString();
+    }
+}
---
+++ IntroductiontoScriptingLaura/Introduction to Scripting Part 1/README.txt
@@ -0,0 +1,5 @@
+He implementat completament tant la part 1 del tutorial com la 2.
+A més d'això he afegit els següents elements:
+- Timer: un compte enrere que quan acaba també acaba el joc. 
+- Música de fons.
+- Fum que surt dels Molins: amb un sistema de partícules he simulat que surt fum dels molins.  
